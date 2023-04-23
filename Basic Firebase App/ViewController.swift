16c7f1e8dbe9c2717bbd23050c110edd1250030e
Ethan <ethan.laudick24@d155.org>
2023-04-23 16:25:57 -0500

    Initial Commit

---
+++ Basic Firebase App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic Firebase App
+//
+//  Created by Scott Laudick on 4/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
